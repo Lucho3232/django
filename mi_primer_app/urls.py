4fa29c090180c204e496dfd846cb65a90f54b9d7
Lucho3232 <luchobata32@gmail.com>
2025-06-24 20:12:47 -0300

    creamos nuestra primer app

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from ejemplo_django.views import saludo
+from .views import saludo
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mi-primer-app/', include('mi_primer_app.urls')),
+    
+    path('hola-mundo/',saludo),
     #path('hola-mundo/',saludo),
 ]
